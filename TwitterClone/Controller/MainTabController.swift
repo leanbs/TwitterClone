a3cc36e0af8211ee42ee288c02a4588ca28b2960
leanbs <davin.yu1110@gmail.com>
2020-03-05 19:04:50 +0700

    delete storyboard, init repo

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterClone
 //
 //  Created by Davin on 05/03/20.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
     }
-
-
 }
-
